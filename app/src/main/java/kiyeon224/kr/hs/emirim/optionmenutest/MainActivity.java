9595ac9faae0380a42144fbdc0bc193a7b77f53a
kiyeon224 <kiyeon224@naver.com>
2017-05-23 19:56:04 +0900

    #4 Option Menu Event Handling

--- app/src/main/java/kiyeon224/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/kiyeon224/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -1,16 +1,24 @@
 package kiyeon224.kr.hs.emirim.optionmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear = (LinearLayout)findViewById(R.id.linear);
+        but = (Button)findViewById(R.id.but_ok);
     }
 
     @Override
@@ -19,5 +27,31 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater mInflater = getMenuInflater();
         mInflater.inflate(R.menu.menu1, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.rgb(220, 220, 255));
+                return true;
+            case R.id.item_pink:
+                linear.setBackgroundColor(Color.rgb(255, 220, 220));
+                return true;
+            case R.id.item_yellow:
+                linear.setBackgroundColor(Color.rgb(255, 255, 80));
+                return true;
+            case R.id.item_but_rotate:
+                //but.setRotation(45);
+                but.setRotation(but.getRotation() + 45);
+                return true;
+            case R.id.item_but_size:
+                //but.setScaleX(10);
+                //but.setScaleY(10);
+                but.setScaleX(but.getScaleX() * 2);
+                return true;
+        }
+        return false;
     }
 }
